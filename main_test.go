8900f6a9ecebb92b2fa390510fe33b7e9e2305a2
Konstantin8105 <konstantin8105@gmail.com>
2018-02-06 13:36:19 +0300

    add test

--- main_test.go
+++ main_test.go
@@ -32,6 +32,35 @@ func TestValues(t *testing.T) {
 	}
 }
 
+// unionValuesAndArray - C code example:
+// union {
+// 		long i;
+// 		int  j[2];
+// };
+type unionValuesAndArray struct {
+	memory [8]byte
+}
+
+func TestValuesAndArray(t *testing.T) {
+	var uni unionValuesAndArray
+
+	var inp int64 = -1234567890
+	uni.memory = *((*[8]byte)(unsafe.Pointer(&inp)))
+	var ind [2]int32
+	ind = *((*[2]int32)(unsafe.Pointer(&uni.memory)))
+	var res int64
+	res = *((*int64)(unsafe.Pointer(&ind)))
+
+	// Output
+	fmt.Printf("inp = %+v\n", inp)
+	fmt.Printf("ind = %+v\n", ind)
+	fmt.Printf("uni = %+v\n", uni)
+	fmt.Printf("res = %+v\n", res)
+	if res != inp {
+		t.Fatalf("Result is not same. %v != %v", inp, res)
+	}
+}
+
 // unionValuesByInterface - C code example:
 // union {
 // 		long i;
