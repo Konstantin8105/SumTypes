00a71c2b1d3cf046aa1e4d0e5f7c263a03bced9f
Konstantin8105 <konstantin8105@gmail.com>
2018-02-06 16:14:50 +0300

    add types

--- main_test.go
+++ main_test.go
@@ -1,4 +1,4 @@
-package main_test
+package main
 
 import (
 	"fmt"
@@ -77,7 +77,9 @@ func TestPointers(t *testing.T) {
 	uni.memory = unsafe.Pointer(&inp)
 	var ind *[2]int32
 	ind = (*[2]int32)(uni.memory)
-	ind[0] += 12222 // changes
+	//ind[0] += 12222 // changes
+	(*[2]int32)(uni.memory)[0] += 12222
+
 	var res int64
 	res = *((*int64)(unsafe.Pointer(ind)))
 
@@ -89,4 +91,30 @@ func TestPointers(t *testing.T) {
 	if res != inp {
 		t.Fatalf("Result is not same. %v != %v", inp, res)
 	}
+}
+
+type unionGo struct {
+	memory unsafe.Pointer
+}
+
+var un unionGo
+
+func GoInit() {
+	var m [200]byte
+	un.memory = unsafe.Pointer(&m)
+	(*[2]int32)(un.memory)[0] = -1234
+	(*[2]int32)(un.memory)[1] = 4556
+}
+
+func TestCtoGo(t *testing.T) {
+	CInit()
+	GoInit()
+
+	fmt.Println("0: ", GetI0(), (*[2]int32)(un.memory)[0]) // int
+	fmt.Println("1: ", GetI1(), (*[2]int32)(un.memory)[1]) // int
+	fmt.Println("2: ", GetL(), *(*int64)(un.memory))       // long
+	fmt.Println("3: ", GetC(), *(*[8]uint16)(un.memory))   // char[8]
+	fmt.Println("4: ", GetI(), *(*[2]int32)(un.memory))    // int[2]
+	fmt.Println("5: ", GetSh(), *(*[16]uint8)(un.memory))  // short[4]
+	fmt.Println("6: ", GetD(), *(*float64)(un.memory))     // double
 }
